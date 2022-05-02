200b8e9df27e6471b2cc42607422b7ee1d8311c2
Michael Diaz <103291795+boofG0D@users.noreply.github.com>
2022-05-01 17:46:03 -0700

    create

--- skill.java
+++ skill.java
@@ -1,1 +1,5 @@
-
+public class skill{
+    public static int addition(int n, int m){
+        return n + m;
+    }
+}
---
+++ skillTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skillTest {
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
